8805636dee5fed46e97704b7f188970d39d49565
PabloLv1 <jose.vasquez@ucrso.info>
2021-12-01 09:21:39 -0600

    Comentario de la aplicación

--- AplicaciónPorDefecto/AplicaciónPorDefecto/Program.cs
+++ AplicaciónPorDefecto/AplicaciónPorDefecto/Program.cs
@@ -9,7 +9,7 @@ namespace AplicaciónPorDefecto
     static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        ///  JosePabloVásquez B98315
         /// </summary>
         [STAThread]
         static void Main()
